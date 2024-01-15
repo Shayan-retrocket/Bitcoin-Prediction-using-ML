3c82cdb32dda95e6cba8a1acde999047e5f9f01f
shayan doroodian <shayan@retrocket.io>
2024-01-15 21:18:01 +0200

    mutual_info_regression / features added / CentralizedWeightLayer / R2 score added

--- prediction.py
+++ prediction.py
@@ -2,116 +2,148 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from tabulate import tabulate
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, GRU, SimpleRNN , Conv1D, MaxPooling1D, Bidirectional, Dense, BatchNormalization, Dropout
+from tensorflow.keras.layers import LSTM, GRU, SimpleRNN, Conv1D, MaxPooling1D, Bidirectional, Dense, BatchNormalization, Dropout, Layer
 from scipy.stats import pearsonr
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.callbacks import EarlyStopping , LearningRateScheduler
-
-
-
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.feature_selection import mutual_info_regression, SelectKBest
+from tensorflow.keras.regularizers import l2
 
 btc_data = pd.read_csv('BTC-USD.csv', index_col='Date', parse_dates=True)
 
 btc_data['Open_scaled'] = MinMaxScaler().fit_transform(btc_data[['Open']])
 btc_data['Volume_scaled'] = MinMaxScaler().fit_transform(btc_data[['Volume']])
+btc_data['High_scaled'] = MinMaxScaler().fit_transform(btc_data[['High']])
+btc_data['Low_scaled'] = MinMaxScaler().fit_transform(btc_data[['Low']])
 
 date_series = btc_data.index
 btc_data['Date_sin'] = np.sin(2 * np.pi * date_series.dayofyear / 365)
 btc_data['Date_cos'] = np.cos(2 * np.pi * date_series.dayofyear / 365)
 
-features = ['Close', 'Open_scaled', 'Volume_scaled', 'Date_sin', 'Date_cos']
+features = ['Close', 'Date_sin', 'Open_scaled', 'Volume_scaled', 'Date_cos', 'High_scaled', 'Low_scaled']
 data_scaled = MinMaxScaler().fit_transform(btc_data[features])
+btc_data['Close_scaled'] = MinMaxScaler().fit_transform(btc_data[['Close']])
+
+seq_length = 10 
+epochs = 150
+batch_size = 32
 
 def create_sequences_multi(data, seq_length):
     sequences, targets = [], []
     for i in range(len(data) - seq_length):
-        seq = data[i : i + seq_length]
-        target = data[i + seq_length, 0]  
+        seq = data[i: i + seq_length]
+        target = data[i + seq_length, 0]
         sequences.append(seq)
         targets.append(target)
     return np.array(sequences), np.array(targets)
 
-def lr_schedule(epoch, lr):
-    if epoch < 10:
-        return lr
-    else:
-        return lr * 0.9 
-    
 
-seq_length = 10  
-epochs = 150
-batch_size = 64
+class CentralizedWeightLayer(Layer):
+    def __init__(self, **kwargs):
+        super(CentralizedWeightLayer, self).__init__(**kwargs)
+        self.w = None 
 
-sequences_multi, targets_multi = create_sequences_multi(data_scaled, seq_length)
+    def build(self, input_shape):
+        super(CentralizedWeightLayer, self).build(input_shape)
+        if self.w is None:
+            self.w = self.add_weight(
+                shape=(int(input_shape[-1]),),
+                initializer='ones',
+                name='centralized_weight',
+                trainable=True
+            )
 
+    def call(self, x):
+        return self.w * x
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+targets_multi = MinMaxScaler().fit_transform(btc_data[['Close']])
+
+# Calculate mutual information values
+mutual_info_values = mutual_info_regression(data_scaled.reshape(-1, len(features)), targets_multi)
+
+# Select top-k features
+k = 5  # Choose the number of top features
+selector = SelectKBest(mutual_info_regression, k=k)
+selected_features = selector.fit_transform(data_scaled.reshape(-1, len(features)), targets_multi)
+sequences_multi, targets_multi = create_sequences_multi(selected_features, seq_length)
+
+# Reshape selected features
+selected_features = selected_features.reshape(-1, k)  # Remove seq_length from the reshape
+
+# Split into training and testing sets
+sequences_multi, targets_multi = create_sequences_multi(selected_features, seq_length)
 split_multi = int(0.8 * len(sequences_multi))
 X_train_multi, y_train_multi = sequences_multi[:split_multi], targets_multi[:split_multi]
 X_test_multi, y_test_multi = sequences_multi[split_multi:], targets_multi[split_multi:]
 
-X_train_multi = np.reshape(X_train_multi, (X_train_multi.shape[0], seq_length, len(features)))
-X_test_multi = np.reshape(X_test_multi, (X_test_multi.shape[0], seq_length, len(features)))
+# Now, you can use X_train_multi and X_test_multi in your model training
 
 model_multi_lstm = Sequential()
-
-model_multi_lstm.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(seq_length, len(features))))
+model_multi_lstm.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(seq_length, k)))
 model_multi_lstm.add(MaxPooling1D(pool_size=2))
-
-model_multi_lstm.add(Bidirectional(LSTM(50, return_sequences=True)))
+model_multi_lstm.add(Bidirectional(LSTM(50, input_shape=(seq_length, k) ,return_sequences=True)))
+model_multi_lstm.add(CentralizedWeightLayer())
 model_multi_lstm.add(BatchNormalization())
-model_multi_lstm.add(Dropout(0.2)) 
+model_multi_lstm.add(Dropout(0.1))
 model_multi_lstm.add(Bidirectional(LSTM(50, return_sequences=True)))
+model_multi_lstm.add(CentralizedWeightLayer())
 model_multi_lstm.add(BatchNormalization())
-model_multi_lstm.add(Dropout(0.2))  
+model_multi_lstm.add(Dropout(0.1))
 model_multi_lstm.add(Bidirectional(LSTM(50)))
+model_multi_lstm.add(CentralizedWeightLayer())
 model_multi_lstm.add(BatchNormalization())
-model_multi_lstm.add(Dropout(0.2)) 
-model_multi_lstm.add(Dense(1))
-optimizer_lstm = Adam(learning_rate=0.01)
+model_multi_lstm.add(Dropout(0.1))
+model_multi_lstm.add(Dense(1, kernel_regularizer=l2(0.01)))
+optimizer_lstm = Adam(learning_rate=0.001)
 model_multi_lstm.compile(optimizer=optimizer_lstm, loss='mean_squared_error')
-lr_scheduler_lstm = LearningRateScheduler(lr_schedule)
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+model_multi_gru = Sequential()
+model_multi_gru.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(seq_length, k)))
+model_multi_gru.add(Bidirectional(GRU(50, input_shape=(seq_length,k), return_sequences=True)))
+model_multi_gru.add(BatchNormalization())
+model_multi_gru.add(CentralizedWeightLayer()) 
+model_multi_gru.add(Dropout(0.1))    
+model_multi_gru.add(Bidirectional(GRU(50, return_sequences=True )))
+model_multi_gru.add(BatchNormalization())  
+model_multi_gru.add(CentralizedWeightLayer()) 
+model_multi_gru.add(Dropout(0.1))    
+model_multi_gru.add(Bidirectional(GRU(50)))
+model_multi_gru.add(BatchNormalization())
+model_multi_gru.add(CentralizedWeightLayer()) 
+model_multi_gru.add(Dropout(0.1))    
+model_multi_gru.add(Dense(1,kernel_regularizer=l2(0.01)))
+optimizer_gru = Adam(learning_rate=0.001)
+model_multi_gru.compile(optimizer=optimizer_gru, loss='mean_squared_error')
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
 
-
-
-
-
-model_multi_gru = Sequential()
-model_multi_gru.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(seq_length, len(features))))
-model_multi_gru.add(Bidirectional(GRU(50, input_shape=(seq_length, len(features)), return_sequences=True)))
-model_multi_lstm.add(BatchNormalization())  
-model_multi_gru.add(Bidirectional(GRU(50, return_sequences=True)))
-model_multi_lstm.add(BatchNormalization())  
-model_multi_gru.add(Bidirectional(GRU(50)))
-model_multi_lstm.add(BatchNormalization())  
-model_multi_gru.add(Dense(1))
-optimizer_gru = Adam(learning_rate=0.01)  
-model_multi_gru.compile(optimizer=optimizer_gru, loss='mean_squared_error')
-
-
-
-
-
 model_multi_rnn = Sequential()
-model_multi_rnn.add(SimpleRNN(50, input_shape=(seq_length, len(features)), return_sequences=True))
+model_multi_rnn.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(seq_length, k)))
+model_multi_rnn.add(SimpleRNN(50, input_shape=(seq_length,k), return_sequences=True) )
+model_multi_rnn.add(BatchNormalization())
+model_multi_gru.add(CentralizedWeightLayer()) 
 model_multi_rnn.add(SimpleRNN(50, return_sequences=True))
+model_multi_rnn.add(BatchNormalization())
+model_multi_gru.add(CentralizedWeightLayer()) 
 model_multi_rnn.add(SimpleRNN(50))
-model_multi_rnn.add(Dense(1))
-optimizer_rnn = Adam(learning_rate=0.01)  
+model_multi_rnn.add(Dense(1,kernel_regularizer=l2(0.01)))
+optimizer_rnn = Adam(learning_rate=0.001)  
 model_multi_rnn.compile(optimizer=optimizer_rnn, loss='mean_squared_error')
 
 
 
-history_multi_lstm = model_multi_lstm.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2)
-
-
-history_multi_gru = model_multi_gru.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2)
-
-history_multi_rnn = model_multi_rnn.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2)
+history_multi_lstm = model_multi_lstm.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2 , callbacks=[ early_stopping ])
+history_multi_gru = model_multi_gru.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2 , callbacks=[early_stopping ])
+history_multi_rnn = model_multi_rnn.fit(X_train_multi, y_train_multi, epochs=epochs, batch_size=batch_size, validation_data=(X_test_multi, y_test_multi), verbose=2 )
 
 y_pred_multi_lstm = model_multi_lstm.predict(X_test_multi)
 y_pred_inv_multi_lstm = MinMaxScaler().fit(btc_data[['Close']]).inverse_transform(y_pred_multi_lstm)
@@ -148,18 +180,19 @@ std_dev_lstm = np.std(y_test_inv_multi_lstm - y_pred_inv_multi_lstm)
 std_dev_gru = np.std(y_test_inv_multi_gru - y_pred_inv_multi_gru)
 std_dev_rnn = np.std(y_test_inv_multi_rnn - y_pred_inv_multi_rnn)
 
-
-
+r2_lstm = r2_score(y_test_inv_multi_lstm, y_pred_inv_multi_lstm)
+r2_gru = r2_score(y_test_inv_multi_gru, y_pred_inv_multi_gru)
+r2_rnn = r2_score(y_test_inv_multi_rnn, y_pred_inv_multi_rnn)
 
 
 table = [
-    ['LSTM', rmse_lstm, mae_lstm, mape_lstm, bias_lstm, corr_lstm, std_dev_lstm],
-    ['GRU', rmse_gru, mae_gru, mape_gru, bias_gru, corr_gru, std_dev_gru],
-    ['SimpleRNN', rmse_rnn, mae_rnn, mape_rnn, bias_rnn, corr_rnn, std_dev_rnn]
+    ['LSTM', round(rmse_lstm, 4), round(mae_lstm, 4), round(mape_lstm, 4), round(bias_lstm, 4), round(corr_lstm, 4), round(std_dev_lstm, 4), round(r2_lstm, 4)],
+    ['GRU', round(rmse_gru, 4), round(mae_gru, 4), round(mape_gru, 4), round(bias_gru, 4), round(corr_gru, 4), round(std_dev_gru, 4), round(r2_gru, 4)],
+    ['SimpleRNN', round(rmse_rnn, 4), round(mae_rnn, 4), round(mape_rnn, 4), round(bias_rnn, 4), round(corr_rnn, 4), round(std_dev_rnn, 4), round(r2_rnn, 4)]
 ]
 
+print(tabulate(table, headers=['Method', 'RMSE', 'MAE', 'MAPE', 'Bias', 'Correlation', 'Std Deviation' , 'R2' , 'Accuracy'], tablefmt='pretty'))
 
-print(tabulate(table, headers=['Method', 'RMSE', 'MAE', 'MAPE', 'Bias', 'Correlation', 'Std Deviation'], tablefmt='pretty'))
 
 plt.subplot(3, 1, 1)
 plt.plot(btc_data.index[-len(y_test_inv_multi_lstm):], y_test_inv_multi_lstm, label='Actual Prices', color='blue')
@@ -168,6 +201,7 @@ plt.title('Actual and Predicted Prices (LSTM)')
 plt.xlabel('Date')
 plt.ylabel('Bitcoin Price')
 plt.legend()
+
 
 plt.subplot(3, 1, 2)
 plt.plot(btc_data.index[-len(y_test_inv_multi_gru):], y_test_inv_multi_gru, label='Actual Prices', color='blue')
@@ -185,5 +219,4 @@ plt.xlabel('Date')
 plt.ylabel('Bitcoin Price')
 plt.legend()
 
-plt.tight_layout()
 plt.show()
